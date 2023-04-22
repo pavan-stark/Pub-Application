42912ab27e5ff707cc0ae9d616c69202bd9b5bd6
Pavan Kalyan <118463533+pavan-stark@users.noreply.github.com>
2023-04-22 21:05:45 +0530

    Add files via upload

--- pages/Find_Nearest_Pub.py
+++ pages/Find_Nearest_Pub.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import folium
 from folium import Marker
 from folium.plugins import MarkerCluster
 from streamlit_folium import folium_static
